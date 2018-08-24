4a337598b71014263a0bbfc471c6ad7ba5c1eed1
ScotlandPiggy <ScotlandPiggy@qq.com>
2018-08-24 16:54:16 +0800

    first commit

--- src/api/api.js
+++ src/api/api.js
@@ -51,13 +51,13 @@ axios.interceptors.response.use(
 	})
 
 
-let base = 'http://192.168.1.172/Shop/public/v1'
-let base1 = 'http://192.168.1.172/Shop/public/v2'
-let base3 = 'http://192.168.1.172/Shop/public/v3'
+// let base = 'http://192.168.1.172/Shop/public/v1'
+// let base1 = 'http://192.168.1.172/Shop/public/v2'
+// let base3 = 'http://192.168.1.172/Shop/public/v3'
 
-// let base = 'https://template.geckowing.com/v1'
-// let base1 = 'https://template.geckowing.com/v2'
-// let base3 = 'https://template.geckowing.com/v3'
+let base = 'https://template.geckowing.com/v1'
+let base1 = 'https://template.geckowing.com/v2'
+let base3 = 'https://template.geckowing.com/v3'
 
 export default base
 
