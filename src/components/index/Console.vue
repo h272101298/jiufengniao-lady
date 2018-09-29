b6208b3d41c683c201cf3de075a72793348f1da8
ScotlandPiggy <ScotlandPiggy@qq.com>
2018-09-29 10:18:11 +0800

    first commit

--- src/components/index/Console.vue
+++ src/components/index/Console.vue
@@ -2,79 +2,89 @@
   <div>
     <el-row class="warp" style="padding:20px 0 0 20px;width:90%;">
       <el-col>
-        <el-row :gutter="20" class="mgb20">
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
+        <el-row :gutter="20" class="dspf">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
             <div class="grid-content grid-con-1" @click="gogood">
               <i class="el-icon-goods grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.productCount}}</div>
-                <div>商品数量</div>
+                <div class="counttext">商品数量</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
             <div class="grid-content grid-con-5" @click="gogood">
               <i class="el-icon-loading grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.reviewProductCount}}</div>
-                <div>待审核商品</div>
+                <div class="counttext">待审核商品</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
             <div class="grid-content grid-con-2" @click="goorder">
               <i class="el-icon-tickets grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.todayOrderCount}}</div>
-                <div>今日订单</div>
+                <div class="counttext">今日订单</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
             <div class="grid-content grid-con-3" @click="goorder">
               <i class="el-icon-sold-out grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.todaySalesCount}}</div>
-                <div>今日销量</div>
+                <div class="counttext">今日销量</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
             <div class="grid-content grid-con-4" @click="gouser">
               <i class="el-icon-upload2 grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.todayUserCount}}</div>
-                <div>今日新增用户</div>
+                <div class="counttext">今日新增用户</div>
+              </div>
+            </div>
+          </el-card>
+
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
+            <div class="grid-content grid-con-6" @click="gouser">
+              <i class="el-icon-view grid-con-icon"></i>
+              <div class="grid-cont-right">
+                <div class="grid-num">{{countdata.loginCount}}</div>
+                <div class="counttext">今日浏览用户</div>
               </div>
             </div>
           </el-card>
         </el-row>
 
-        <el-row :gutter="20" class="mgb20">
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
-            <div class="grid-content grid-con-4" @click="gotxls">
+        <el-row :gutter="20" class="dspf">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
+            <div class="grid-content grid-con-7" @click="gotxls">
               <i class="el-icon-date grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.storeAmount}}</div>
-                <div>总营业额</div>
+                <div class="counttext">总营业额</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
-            <div class="grid-content grid-con-4" @click="gotxls">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
+            <div class="grid-content grid-con-7" @click="gotxls">
               <i class="el-icon-star-on grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.amount}}</div>
-                <div>余额</div>
+                <div class="counttext">余额</div>
               </div>
             </div>
           </el-card>
-          <el-card shadow="hover" :body-style="{padding: '0px'}">
-            <div class="grid-content grid-con-4" @click="gotxls">
+          <el-card shadow="hover" :body-style="{padding: '0px'}" class="mb20">
+            <div class="grid-content grid-con-7" @click="gotxls">
               <i class="el-icon-success grid-con-icon"></i>
               <div class="grid-cont-right">
                 <div class="grid-num">{{countdata.withdrawAmount}}</div>
-                <div>已提现</div>
+                <div class="counttext">已提现</div>
               </div>
             </div>
           </el-card>
@@ -103,6 +113,7 @@
           withdrawAmount:0,
           amount:0,
           storeAmount:0,
+          loginCount:0
         }
       }
     },
@@ -138,9 +149,15 @@
 </script>
 
 <style scoped>
-.mgb20{
+.dspf{
   display: flex;
   align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+
+.mb20{
+  margin-bottom: 20px;
 }
 .el-row {
   margin-bottom: 20px;
@@ -187,6 +204,18 @@
 .grid-con-1 .grid-num {
   color: rgb(45, 140, 240);
 }
+.grid-con-2 .grid-con-icon {
+  background: rgb(45, 140, 140);
+}
+.grid-con-2 .grid-num {
+  color: rgb(45, 140, 140);
+}
+.grid-con-3 .grid-con-icon {
+  background: rgb(100, 213, 214);
+}
+.grid-con-3 .grid-num {
+  color: rgb(100, 213, 214);
+}
 .grid-con-4 .grid-con-icon {
   background: rgb(100, 213, 114);
 }
@@ -199,16 +228,21 @@
 .grid-con-5 .grid-num {
   color: rgb(242, 94, 67);
 }
-.grid-con-2 .grid-con-icon {
-  background: rgb(45, 140, 140);
+.grid-con-6 .grid-con-icon {
+  background: rgb(0,191,255);
 }
-.grid-con-2 .grid-num {
-  color: rgb(45, 140, 140);
+.grid-con-6 .grid-num {
+  color: rgb(0,191,255);
 }
-.grid-con-3 .grid-con-icon {
-  background: rgb(100, 213, 214);
+
+.grid-con-7 .grid-con-icon {
+  background: rgb(255,165,50);
 }
-.grid-con-3 .grid-num {
-  color: rgb(100, 213, 214);
+.grid-con-7 .grid-num {
+  color: rgb(255,165,50);
+}
+
+.counttext{
+  font-size: 16px;
 }
 </style>
