e774b07dfbabf7a34caf78d43b019707b2aed2cf
scotlandpiggy <ScotlandPiggy@qq.com>
2018-12-06 17:25:15 +0800

    first commit

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -1,11 +1,11 @@
 const qiniu={
 
-	token:'0nvFBGk3MHeRLD3jNt8a89v0dV9en_9MfBKouW4A:qAEhidb6XKEPu4fE5XMav5A9g50=:eyJzY29wZSI6InpoZW5iYW8iLCJkZWFkbGluZSI6MjE3MDMxNTc1OX0=',
+	token:'og7Mlfh-qhsdYS2V5-1s2cmeYEinlnJkHKoLwZmg:88KwI1o_stLYTjnNIpWQltGEMew=:eyJzY29wZSI6InNoYW5nY2hlbmdtdWJhbiIsImRlYWRsaW5lIjoxOTMxODE0ODk4fQ==',
 
 	upurl:'https://upload-z2.qiniup.com',
 
 
-	showurl:'https://bao.spzbt.com/'
+	showurl:'https://sheng.qdbnm.com/'
 	// showurl:'https://qu.qdbnm.com/'
 	// showurl:'http://pam213sin.bkt.clouddn.com/'
 };
