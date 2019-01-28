f09089bcfbf4732c47b17ebe141048bdce6d6bc9
Your Name <you@example.com>
2019-01-28 16:26:49 +0800

    1

--- src/api/qiniu.js
+++ src/api/qiniu.js
@@ -1,11 +1,11 @@
 const qiniu={
 
-	token:'og7Mlfh-qhsdYS2V5-1s2cmeYEinlnJkHKoLwZmg:88KwI1o_stLYTjnNIpWQltGEMew=:eyJzY29wZSI6InNoYW5nY2hlbmdtdWJhbiIsImRlYWRsaW5lIjoxOTMxODE0ODk4fQ==',
+	token:'v8HMJl-CM2EBkkZVsRsVKsYKI5vv-NHP4O9h4MTX:2psFycD0gxZ_3eb8OhnRsgB-NJ0=:eyJzY29wZSI6InB5d29tYW4iLCJkZWFkbGluZSI6MTkzMjUwMjY3MX0=',
 
 	upurl:'https://upload-z2.qiniup.com',
 
 
-	showurl:'https://sheng.qdbnm.com/'
+	showurl:'https://qi.pywoman.com/'
 	// showurl:'https://qu.qdbnm.com/'
 	// showurl:'http://pam213sin.bkt.clouddn.com/'
 };
